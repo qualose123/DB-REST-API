b4d2a93fecaf06580e4421ccf467ed21438b04a1
qualose123 <jonhyzicaos@gmail.com>
2022-07-19 10:37:49 -0300

    .

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 8000;
 
 server.use(middlewares)
 server.use(router)
-server.listen(8000, () => {
-  console.log('JSON Server is running')
-})
+server.listen(port)
